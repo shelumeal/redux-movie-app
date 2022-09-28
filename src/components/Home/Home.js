b7a6cdada72584a4082a9976bff6a1237dee9c8d
Shelumiel <sssharon7@gmail.com>
2022-09-28 13:06:40 +0800

    Header Component with SCSS

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import user from "../../images/user.png";
+import "../Header/Header.scss";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="header">
+      <Link to="/">
+        <div className="logo">Movie App</div>
+      </Link>
+      <div className="user-image">
+        <img src={user} alt="user"></img>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
