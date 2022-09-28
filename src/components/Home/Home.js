15db4bc077d4b25c9379324701415f735220e826
Shelumiel <sssharon7@gmail.com>
2022-09-28 13:31:52 +0800

    Add Home Component with SCSS

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,13 @@
 import React from "react";
+import MovieListing from "../MovieListing/MovieListing";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <div className="banner-img"></div>
+      <MovieListing />
+    </div>
+  );
 }
 
 export default Home;
