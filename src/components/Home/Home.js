5f5e7ffabe3ffeea1ee1d9292d86c66b36207155
Shelumiel <sssharon7@gmail.com>
2022-09-28 13:57:07 +0800

    Use Axios for API Call

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import MovieListing from "../MovieListing/MovieListing";
+import movieApi from "../../common/api/movieApi";
+import { APIKey } from "../../common/api/movieApiKey";
 
 function Home() {
+  useEffect(() => {
+    const moveText = "Harry";
+    const fetchMovies = async () => {
+      const response = await movieApi
+        .get(`?apiKey=${APIKey}&s=${moveText}&type=movie`)
+        .then((response) => {
+          console.log(response);
+        })
+        .catch((err) => {
+          console.log("Error :", err);
+        });
+    };
+
+    fetchMovies();
+  }, []);
   return (
     <div>
       <div className="banner-img"></div>
