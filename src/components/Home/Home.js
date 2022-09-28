7642868cddd00e8b339e2da2472abed803af9218
Shelumiel <sssharon7@gmail.com>
2022-09-28 13:21:39 +0800

    Add Footer Component with SCSS

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,19 +1,7 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import user from "../../images/user.png";
-import "../Header/Header.scss";
 
 function Home() {
-  return (
-    <div className="header">
-      <Link to="/">
-        <div className="logo">Movie App</div>
-      </Link>
-      <div className="user-image">
-        <img src={user} alt="user"></img>
-      </div>
-    </div>
-  );
+  return <div>Home</div>;
 }
 
 export default Home;
