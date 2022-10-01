fd45e1b60ff46526a186097cb82941af37ec2b44
Shelumiel <sssharon7@gmail.com>
2022-10-01 12:12:11 +0800

    useSelector to access state

--- src/components/MovieListing/MovieListing.js
+++ src/components/MovieListing/MovieListing.js
@@ -1,6 +1,10 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { getAllMovies } from "../../features/movies/movieSlice";
 
 function MovieListing() {
+  const movies = useSelector(getAllMovies);
+  console.log(movies);
   return <div>Movie Listing</div>;
 }
 
