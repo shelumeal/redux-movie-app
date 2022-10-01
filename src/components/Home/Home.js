211ffeda1d56a4a7c2ce9222cf4ca11542b38e31
Shelumiel <sssharon7@gmail.com>
2022-10-01 12:06:10 +0800

    useDispatch to dispatch action

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,15 +2,19 @@ import React, { useEffect } from "react";
 import MovieListing from "../MovieListing/MovieListing";
 import movieApi from "../../common/api/movieApi";
 import { APIKey } from "../../common/api/movieApiKey";
+import { useDispatch } from "react-redux";
+import { addMovies } from "../../features/movies/movieSlice";
 
 function Home() {
+  const moveText = "Harry";
+  const dispatch = useDispatch();
+
   useEffect(() => {
-    const moveText = "Harry";
     const fetchMovies = async () => {
       const response = await movieApi
         .get(`?apiKey=${APIKey}&s=${moveText}&type=movie`)
         .then((response) => {
-          console.log(response);
+          dispatch(addMovies(response.data));
         })
         .catch((err) => {
           console.log("Error :", err);
