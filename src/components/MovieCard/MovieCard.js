653188dafd30e9ec3a4e77e902eab69f553c0d1e
Shelumiel <sssharon7@gmail.com>
2022-10-03 12:12:17 +0800

    Movie Listing Component with SCSS

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -1,7 +1,22 @@
 import React from "react";
 
-function MovieCard() {
-  return <div>Movie Card</div>;
+function MovieCard(props) {
+  const { data } = props;
+  return (
+    <div className="card-item">
+      <div className="card-inner">
+        <div className="card-top">
+          <img src={data.Poster} alt={data.Title} />
+        </div>
+        <div className="card-bottom">
+          <div className="card-info">
+            <h4>{data.Title}</h4>
+            <p>{data.Year}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default MovieCard;
